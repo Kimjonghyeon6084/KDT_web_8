3bfffd1de60fad7aaf7f36c019e11004e61938e4
kimjonghyeon6084 <ssoert123@naver.com>
2023-07-29 17:44:29 +0900

    4week_3day

--- 4week/0727/index_2.js
+++ 4week/0727/index_2.js
@@ -1,17 +1,17 @@
-const http = require("http");
-const fs = require("fs").promises;
+// const http = require("http");
+// const fs = require("fs").promises;
 
-const server = http.createServer(async function (req, res) {
-  try {
-    const data = await fs.readFile("/index.html");
-    res.writeHead(200);
-    res.end(data);
-  } catch (err) {
-    console.error(err);
-    res.writeHead(404);
-    res.end(err.message);
-  }
-});
+// const server = http.createServer(async function (req, res) {
+//   try {
+//     const data = await fs.readFile("./0727/index.html");
+//     res.writeHead(200);
+//     res.end(data);
+//   } catch (err) {
+//     console.error(err);
+//     res.writeHead(404);
+//     res.end(err.message);
+//   }
+// });
 
 // const server = http.createServer(function (req, res) {
 //   res.writeHead("200");
@@ -21,3 +21,15 @@ const server = http.createServer(async function (req, res) {
 // server.listen(8000, function () {
 //   console.log("8000포트로 서버 실행");
 // });
+
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.get("/", function (req, res) {
+  res.send("hello express");
+});
+
+app.listen(PORT, function () {
+  console.log("Listening on port ${PORT}! http://localhost:${PORT}");
+});
