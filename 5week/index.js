4bd703ee09c52ed3f262aa76fd30164fa0f4b94b
kimjonghyeon6084 <ssoert123@naver.com>
2023-08-01 08:09:12 +0900

    test

--- 5week/index.js
+++ 5week/index.js
@@ -12,22 +12,22 @@ app.set("views", "./views");
 //router
 app.get("/", (req, res) => {
   // res.send("hello");
-  res.render("index", { title: "실습 1. get으로 정보 받기" });
+  res.render("index2", { title: "실습 2. post으로 정보 받기" });
 });
 
-app.get("./getForm", (req, res) => {
-  console.log(req.query);
-  res.render("result", {
-    title: "GET요청 폼 결과 확인하기",
-    userInfo: req.query,
-    //userInfo: {id : '', pw: ''}
-  });
-});
+// app.get("/getForm", (req, res) => {
+//   console.log(req.query);
+//   res.render("result", {
+//     title: "GET요청 폼 결과 확인하기",
+//     userInfo: req.query,
+//     //userInfo: {id : '', pw: ''}
+//   });
+// });
 
-app.post("./postForm", (req, res) => {
+app.post("/postForm", (req, res) => {
   console.log(req.body);
-  res.render("result", {
-    title: "GET요청 폼 결과 확인하기",
+  res.render("result2", {
+    title: "POST요청 폼 결과 확인하기",
     userInfo: req.body,
     //userInfo: {id : '', pw: ''}
   });
